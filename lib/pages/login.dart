49769939d92bd1ed3fa97c15d2f48c187956abca
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-12 18:21:20 -0300

    Commit 3

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -35,28 +35,27 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(toolbarHeight: 0),
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: Formulario(
-              formKey: formKey,
-              emailController: _emailController,
-              senhaController: _senhaController,
-              onPressedEntrar: onSubmit,
-              onPressedEsqueceuSenha: esqueceuSenha,
-              onPressedNovoUsuario: novoUsuario,
-            ),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: _FormularioLogin(
+          width: 500,
+          formKey: formKey,
+          emailController: _emailController,
+          senhaController: _senhaController,
+          onPressedEntrar: onSubmit,
+          onPressedEsqueceuSenha: esqueceuSenha,
+          onPressedNovoUsuario: novoUsuario,
+        ),
       ),
     );
   }
 }
 
-class Formulario extends StatelessWidget {
-  const Formulario({
+class _FormularioLogin extends StatelessWidget {
+  const _FormularioLogin({
+    // ignore: unused_element
     super.key,
     required this.formKey,
+    required this.width,
     required this.emailController,
     required this.senhaController,
     required this.onPressedEntrar,
@@ -64,6 +63,7 @@ class Formulario extends StatelessWidget {
     required this.onPressedNovoUsuario,
   });
 
+  final double width;
   final GlobalKey<FormState> formKey;
   final TextEditingController emailController;
   final TextEditingController senhaController;
@@ -73,84 +73,82 @@ class Formulario extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            const Center(
-              child: Text(
-                "Login",
-                style: TextStyle(fontSize: 32),
-              ),
-            ),
-            CampoTexto(
-              exibeLabel: true,
-              label: "Nome",
-              validator: validaCampoVazio,
-              keyboardType: TextInputType.multiline,
-              controller: emailController,
-              hintText: "Digite o titulo da tarefa",
-              obscureText: false,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: CampoTexto(
-                exibeLabel: true,
-                label: "Senha",
-                validator: validaCampoVazio,
-                // keyboardType: TextInputType.text,
-                controller: senhaController,
-                hintText: "Digite o titulo da tarefa",
-                obscureText: true,
-              ),
-            ),
-            Botao(
-              onPressed: onPressedEntrar,
-              fontColor: Colors.white,
-              label: "Entrar",
-              backgroundColor: Colors.blueGrey,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: TextButton(
-                onPressed: onPressedEsqueceuSenha,
-                child: const Text(
-                  "Esqueceu a senha?",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.blue,
-                    decoration: TextDecoration.underline,
-                    decorationColor: Colors.blue,
-                  ),
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return Center(
+      child: SizedBox(
+        width: width,
+        child: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Form(
+            key: formKey,
+            child: Column(
               children: [
-                Text(
-                  "Ainda não tem conta?",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.grey.shade600,
+                const Center(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 32),
                   ),
                 ),
-                const SizedBox(width: 4),
-                TextButton(
-                  onPressed: onPressedNovoUsuario,
-                  child: const Text(
-                    "Faça seu cadastro!",
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.blueAccent,
+                CampoTexto(
+                  exibeLabel: true,
+                  label: "Nome",
+                  validator: validaCampoVazio,
+                  keyboardType: TextInputType.multiline,
+                  controller: emailController,
+                  hintText: "Digite o titulo da tarefa",
+                  obscureText: false,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: CampoTexto(
+                    exibeLabel: true,
+                    label: "Senha",
+                    validator: validaCampoVazio,
+                    // keyboardType: TextInputType.text,
+                    controller: senhaController,
+                    hintText: "Digite o titulo da tarefa",
+                    obscureText: true,
+                  ),
+                ),
+                Botao(
+                  onPressed: onPressedEntrar,
+                  fontColor: Colors.white,
+                  label: "Entrar",
+                  backgroundColor: Colors.blueGrey,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: TextButton(
+                    onPressed: onPressedEsqueceuSenha,
+                    child: const Text(
+                      "Esqueceu a senha?",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.blue,
+                      ),
                     ),
                   ),
                 ),
+                TextButton(
+                  onPressed: onPressedNovoUsuario,
+                  child: const Text.rich(TextSpan(
+                    text: "Ainda não tem conta? ",
+                    children: [
+                      TextSpan(
+                        text: "Faça seu cadastro!",
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                    ],
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                  )),
+                ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
