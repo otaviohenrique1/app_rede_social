dac0a71aead33079029e6f85d0a7111ee216abb1
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-15 12:03:05 -0300

    Commit 5

--- lib/pages/novo_usuario.dart
+++ lib/pages/novo_usuario.dart
@@ -5,22 +5,27 @@ import 'package:flutter/material.dart';
 
 import '../components/campo_texto.dart';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+class NovoUsuario extends StatefulWidget {
+  const NovoUsuario({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
+  State<NovoUsuario> createState() => _NovoUsuarioState();
 }
 
-class _LoginState extends State<Login> {
+class _NovoUsuarioState extends State<NovoUsuario> {
   var formKey = GlobalKey<FormState>();
+  final TextEditingController _nomeController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _senhaController = TextEditingController();
+  final TextEditingController _confirmarSenhaController =
+      TextEditingController();
 
   @override
   void dispose() {
+    _nomeController.dispose();
     _emailController.dispose();
     _senhaController.dispose();
+    _confirmarSenhaController.dispose();
     super.dispose();
   }
 
@@ -42,61 +47,61 @@ class _LoginState extends State<Login> {
             return _FormularioLogin(
               width: 1000,
               formKey: formKey,
+              nomeController: _nomeController,
               emailController: _emailController,
               senhaController: _senhaController,
-              onPressedEntrar: onSubmit,
-              onPressedEsqueceuSenha: esqueceuSenha,
-              onPressedNovoUsuario: novoUsuario,
+              confirmarSenhaController: _confirmarSenhaController,
+              onPressedCadastrar: onSubmit,
             );
           } else if (constraints.maxWidth > 1200) {
             return _FormularioLogin(
               width: 800,
               formKey: formKey,
+              nomeController: _nomeController,
               emailController: _emailController,
               senhaController: _senhaController,
-              onPressedEntrar: onSubmit,
-              onPressedEsqueceuSenha: esqueceuSenha,
-              onPressedNovoUsuario: novoUsuario,
+              confirmarSenhaController: _confirmarSenhaController,
+              onPressedCadastrar: onSubmit,
             );
           } else if (constraints.maxWidth > 1000) {
             return _FormularioLogin(
               width: 700,
               formKey: formKey,
+              nomeController: _nomeController,
               emailController: _emailController,
               senhaController: _senhaController,
-              onPressedEntrar: onSubmit,
-              onPressedEsqueceuSenha: esqueceuSenha,
-              onPressedNovoUsuario: novoUsuario,
+              confirmarSenhaController: _confirmarSenhaController,
+              onPressedCadastrar: onSubmit,
             );
           } else if (constraints.maxWidth > 800) {
             return _FormularioLogin(
               width: 600,
               formKey: formKey,
+              nomeController: _nomeController,
               emailController: _emailController,
               senhaController: _senhaController,
-              onPressedEntrar: onSubmit,
-              onPressedEsqueceuSenha: esqueceuSenha,
-              onPressedNovoUsuario: novoUsuario,
+              confirmarSenhaController: _confirmarSenhaController,
+              onPressedCadastrar: onSubmit,
             );
           } else if (constraints.maxWidth > 600) {
             return _FormularioLogin(
               width: 500,
               formKey: formKey,
+              nomeController: _nomeController,
               emailController: _emailController,
               senhaController: _senhaController,
-              onPressedEntrar: onSubmit,
-              onPressedEsqueceuSenha: esqueceuSenha,
-              onPressedNovoUsuario: novoUsuario,
+              confirmarSenhaController: _confirmarSenhaController,
+              onPressedCadastrar: onSubmit,
             );
           } else {
             return _FormularioLogin(
-              width: 400,
+              width: 300,
               formKey: formKey,
+              nomeController: _nomeController,
               emailController: _emailController,
               senhaController: _senhaController,
-              onPressedEntrar: onSubmit,
-              onPressedEsqueceuSenha: esqueceuSenha,
-              onPressedNovoUsuario: novoUsuario,
+              confirmarSenhaController: _confirmarSenhaController,
+              onPressedCadastrar: onSubmit,
             );
           }
         },
@@ -113,18 +118,18 @@ class _FormularioLogin extends StatelessWidget {
     required this.width,
     required this.emailController,
     required this.senhaController,
-    required this.onPressedEntrar,
-    required this.onPressedEsqueceuSenha,
-    required this.onPressedNovoUsuario,
+    required this.onPressedCadastrar,
+    required this.nomeController,
+    required this.confirmarSenhaController,
   });
 
   final double width;
   final GlobalKey<FormState> formKey;
   final TextEditingController emailController;
+  final TextEditingController nomeController;
   final TextEditingController senhaController;
-  final Function() onPressedEntrar;
-  final Function() onPressedEsqueceuSenha;
-  final Function() onPressedNovoUsuario;
+  final TextEditingController confirmarSenhaController;
+  final Function() onPressedCadastrar;
 
   @override
   Widget build(BuildContext context) {
@@ -148,9 +153,18 @@ class _FormularioLogin extends StatelessWidget {
                     exibeLabel: true,
                     label: "Nome",
                     validator: validaCampoVazio,
-                    keyboardType: TextInputType.multiline,
+                    keyboardType: TextInputType.text,
+                    controller: nomeController,
+                    hintText: "Digite o seu nome",
+                    obscureText: false,
+                  ),
+                  CampoTexto(
+                    exibeLabel: true,
+                    label: "E-mail",
+                    validator: validaCampoVazio,
+                    keyboardType: TextInputType.emailAddress,
                     controller: emailController,
-                    hintText: "Digite o titulo da tarefa",
+                    hintText: "Digite o seu email",
                     obscureText: false,
                   ),
                   Padding(
@@ -159,48 +173,29 @@ class _FormularioLogin extends StatelessWidget {
                       exibeLabel: true,
                       label: "Senha",
                       validator: validaCampoVazio,
-                      // keyboardType: TextInputType.text,
+                      keyboardType: TextInputType.text,
                       controller: senhaController,
-                      hintText: "Digite o titulo da tarefa",
+                      hintText: "Digite a sua senha",
+                      obscureText: true,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: CampoTexto(
+                      exibeLabel: true,
+                      label: "Repita a senha",
+                      validator: validaCampoVazio,
+                      keyboardType: TextInputType.text,
+                      controller: confirmarSenhaController,
+                      hintText: "Confirme a sua senha",
                       obscureText: true,
                     ),
                   ),
                   Botao(
-                    onPressed: onPressedEntrar,
-                    label: "Entrar",
+                    onPressed: onPressedCadastrar,
+                    label: "Cadastrar",
                     fontColor: ThemesColors.white,
                     backgroundColor: ThemesColors.blueGrey,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    child: TextButton(
-                      onPressed: onPressedEsqueceuSenha,
-                      child: const Text(
-                        "Esqueceu a senha?",
-                        style: TextStyle(
-                          fontSize: 18,
-                          decoration: TextDecoration.underline,
-                          color: ThemesColors.blue,
-                          decorationColor: ThemesColors.blue,
-                        ),
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: onPressedNovoUsuario,
-                    child: const Text.rich(TextSpan(
-                      text: "Ainda não tem conta? ",
-                      children: [
-                        TextSpan(
-                          text: "Faça seu cadastro!",
-                          style: TextStyle(color: ThemesColors.blueAccent),
-                        ),
-                      ],
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: ThemesColors.grey,
-                      ),
-                    )),
                   ),
                 ],
               ),
